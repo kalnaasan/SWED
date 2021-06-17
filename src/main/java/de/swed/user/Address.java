ec510b1687ddc4df552876b93db4e91adb55e028
Kaddour Alnaasan <kaddour@alnaasan.de>
2021-06-17 23:39:44 +0200

    create Address

---
+++ src/main/java/de/swed/user/Address.java
@@ -0,0 +1,49 @@
+package de.swed.user;
+
+public class Address {
+    private Long id;
+    private String streetName;
+    private int streetNumber;
+    private int postCode;
+    private String city;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public int getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(int postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
