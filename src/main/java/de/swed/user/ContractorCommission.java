e36f6d45629e98de2b3f37830103cd2fa8e46629
Kaddour Alnaasan <kaddour@alnaasan.de>
2021-07-04 17:36:47 +0200

    rename ContractorCommission

---
+++ src/main/java/de/swed/user/ContractorCommission.java
@@ -0,0 +1,18 @@
+package de.swed.user;
+
+public class ContractorCommission {
+    private long id;
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private void calcPermission() {
+    }
+
+}
